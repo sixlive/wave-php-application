de6a3b6361f56a2a0246354da6631d5b367cb109
sixlive <tj@tjmiller.co>
2018-09-19 10:01:09 -0700

    wip

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\ReadingLog;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,23 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        ReadingLog::create([
+            'book_title' => 'Buddha at Bedtime',
+            'book_author' => 'Dharmachari Nagaraja',
+            'read_date' => '2018-09-01'
+        ]);
+
+        ReadingLog::create([
+            'book_title' => 'Where the Wild Things Are',
+            'book_author' => 'Maurice Sendak',
+            'read_date' => '2018-09-01'
+        ]);
+
+        ReadingLog::create([
+            'book_title' => 'The Big Book of Paw Patrol',
+            'book_author' => 'Golden Books',
+            'read_date' => '2018-08-31'
+        ]);
+
     }
 }
